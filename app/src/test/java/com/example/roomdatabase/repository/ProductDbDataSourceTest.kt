60bd08f5d2ab97f50a8840e2ee89f021d21dc445
Otavio Leao <otavio.leao@iteris.com.br>
2020-07-07 16:01:20 -0300

    unit test viewmodel e repository

--- app/src/test/java/com/example/roomdatabase/repository/ProductDbDataSourceTest.kt
+++ app/src/test/java/com/example/roomdatabase/repository/ProductDbDataSourceTest.kt
@@ -1,5 +1,112 @@
 package com.example.roomdatabase.repository
 
-import org.junit.Assert.*
+import android.content.Context
+import android.os.Build
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.platform.app.InstrumentationRegistry
+import com.example.roomdatabase.data.db.AppDatabase
+import com.example.roomdatabase.data.db.dao.ProductDao
+import com.example.roomdatabase.model.Product
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
-class ProductDbDataSourceTest
+@ExperimentalCoroutinesApi
+@RunWith(RobolectricTestRunner::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1], manifest = Config.NONE)
+class ProductDbDataSourceTest {
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    private var dao: ProductDao = mockk()
+    private var database: AppDatabase = mockk()
+    private var productDbDataSource: ProductDbDataSource = mockk()
+
+
+    @Before
+    fun setUp() {
+        val context: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        database =
+            Room.inMemoryDatabaseBuilder(context, AppDatabase::class.java).allowMainThreadQueries()
+                .build()
+        dao = database.productDao()
+
+        productDbDataSource = ProductDbDataSource(dao)
+
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        database.close()
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+
+    }
+
+    @Test
+    fun `when insertProduct called then it should list bigger zero`() {
+        runBlockingTest {
+            productDbDataSource.insertProduct("teste")
+            val result = productDbDataSource.getAll()
+            assertEquals(1, result.size)
+
+        }
+
+    }
+
+    @Test
+    fun `when markCompleted is called then must mark as complete`() {
+        runBlockingTest {
+           val items = getBaseList()
+            addItems(items)
+
+            productDbDataSource.markCompleted(items[0])
+            val result = productDbDataSource.getAll()
+            assertEquals(1, result[0].completed)
+        }
+
+    }
+
+    @Test
+    fun `when delete product is called then it should deleted product `() {
+        runBlockingTest {
+
+            val item = getBaseList()
+            addItems(item)
+            productDbDataSource.deleteProduct(item[0])
+            val result = productDbDataSource.getAll()
+            assertEquals(2, result.size)
+
+        }
+
+    }
+
+    private fun getBaseList() = listOf(
+        Product(1, "Task 1", 0),
+        Product(2, "Task 3", 0),
+        Product(3, "Task 4", 0)
+    )
+
+    private fun addItems(list: List<Product>) = runBlockingTest {
+        list.forEach {
+            productDbDataSource.insertProduct(it.productName)
+        }
+    }
+}
--- app/src/test/java/com/example/roomdatabase/ui/viewmodel/MainActivityViewModelTest.kt
+++ app/src/test/java/com/example/roomdatabase/ui/viewmodel/MainActivityViewModelTest.kt
@@ -1,5 +1,112 @@
 package com.example.roomdatabase.ui.viewmodel
 
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.roomdatabase.model.Product
+import com.example.roomdatabase.repository.ProductRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 
-class MainActivityViewModelTest
+@ExperimentalCoroutinesApi
+class MainActivityViewModelTest {
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+    private val testDispatcher = TestCoroutineDispatcher()
+
+    private val productRepositoryMock: ProductRepository = mockk()
+    private val listProductsMockk: Observer<List<Product>> = mockk(relaxed = true)
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDown() {
+        Dispatchers.resetMain()
+        testDispatcher.cleanupTestCoroutines()
+    }
+
+    @Test
+    fun `when products is called then it should call repository to fetch products`() {
+        coEvery {
+            productRepositoryMock.insertProduct(any())
+        } returns listProductsMocked()
+        instantiationViewModel().saveProduct("teste")
+        coVerify { productRepositoryMock.insertProduct("teste") }
+
+    }
+
+    @Test
+    fun `when getAll is called it should list all`() {
+        coEvery {
+            productRepositoryMock.getAll()
+        } returns listProductsMocked()
+
+        instantiationViewModel().getAll()
+        coVerify { listProductsMockk.onChanged(listProductsMocked()) }
+    }
+
+    @Test
+    fun `when insertProduct is called it should list count`() {
+        coEvery {
+            productRepositoryMock.insertProduct(any())
+        } returns listProductsMocked()
+
+        instantiationViewModel().saveProduct("any")
+        assertEquals(0, instantiationViewModel().productSize())
+
+    }
+
+    @Test
+    fun `when markCompleted is called it should list`() {
+        coEvery {
+            productRepositoryMock.markCompleted(any())
+        } returns listProductsMocked()
+
+        instantiationViewModel().markCompleted(Product(1, "teste", 1))
+        coVerify { listProductsMockk.onChanged(listProductsMocked()) }
+
+    }
+
+    @Test
+    fun `when delete is called it should list`() {
+        coEvery {
+            productRepositoryMock.deleteProduct(any())
+        } returns listProductsMocked()
+
+        instantiationViewModel().deleteProduct(Product(1, "teste", 1))
+        coVerify { listProductsMockk.onChanged(listProductsMocked()) }
+
+    }
+
+
+    private fun instantiationViewModel(): MainActivityViewModel {
+        val viewModel = MainActivityViewModel(productRepositoryMock)
+        viewModel.listProduct.observeForever(listProductsMockk)
+        return viewModel
+    }
+
+    private fun listProductsMocked(): List<Product> {
+        return listOf(
+            Product(1, "teste1", 1),
+            Product(2, "teste2", 1),
+            Product(3, "teste3", 1)
+        )
+
+    }
+
+}
